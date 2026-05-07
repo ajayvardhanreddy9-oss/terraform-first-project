9dc6700774137f70942c7eb16a4e2c180752a036
ajayvardhanreddy9-oss <ajayvardhanreddy9@gmail.com>
2026-05-07 13:42:40 +0530

    Refactor S3 bucket resource definitions in main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
 }
+
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
+}
+
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -15,27 +18,30 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [aws_s3_bucket_ownership_controls.example]
 
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
+}
 
 resource "aws_s3_bucket_object" "index" {
- bucket = aws_s3_bucket.mybucket.id
-  key    = "index.html"
-  source = "index.html"
-  acl = "public-read"
- content_type = "text/html"
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "index.html"
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
 }
 
 resource "aws_s3_bucket_object" "error" {
- bucket = aws_s3_bucket.mybucket.id
-  key    = "error.html"
-  source = "error.html"
-  acl = "public-read"
- content_type = "text/html"
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "error.html"
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
 }
+
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -46,7 +52,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "error.html"
   }
-depends_on = [ aws_s3_bucket_acl.example ]
+
+  depends_on = [aws_s3_bucket_acl.example]
 }
-
-
