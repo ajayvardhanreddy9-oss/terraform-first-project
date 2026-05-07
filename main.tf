a78f4926688385a4fb822b3d6f1d4bdc5e5cfc56
ajayvardhanreddy9-oss <ajayvardhanreddy9@gmail.com>
2026-05-07 13:07:02 +0530

    Add S3 bucket ownership controls and ACL settings

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
 }
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+
